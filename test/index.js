323dd2f1a0ee98e125ea2d923f9306dda3eef139
Tim Allen <tim@noblesamurai.com>
2017-05-05 10:25:40 +1000

    Fix tests.

--- test/index.js
+++ test/index.js
@@ -1,10 +1,10 @@
 const librato = require('..');
 const nock = require('nock');
 
-const token = '23fe05b7ee166dabe7cfad35c6d09f48c347f37f182d3b01ffecebb8aac2b073';
+const token = 'whatever';
 
 // FIXME(Tim): This is not nocking properly... could it be b/c I am using request-promise-native?
-describe.skip('librato', function () {
+describe('librato', function () {
   beforeEach(function () {
     nock.disableNetConnect();
     nock('https://metrics-api.librato.com').post('/v1/measurements', /.*/);
@@ -20,6 +20,6 @@ describe.skip('librato', function () {
 
   it('inits the metric as a counter', function () {
     this.timeout(10000);
-    return librato({ username: 'devteam+librato@noblesamurai.com', token }, 'test.metric', { person: 'tim' });
+    return librato({ username: 'me@my.com', token }, 'test.metric', { person: 'tim' });
   });
 });
